5e8dfe5432424180a47cdd73e0086af715a71a86
Alumist <codealumist@gmail.com>
2024-12-26 11:02:43 -0800

    color-change

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,16 +1,16 @@
 function About() {
   return (
     <div
-      className='text-2xl bg-gradient-to-r from-indigo-400 via-purple-700 to-pink-400 my-32 p-6 rounded-l sm:mx-9 md:mx-20 lg:mx-40'
+      className='text-2xl bg-indigo-400 my-32 p-6 rounded-l sm:mx-9 md:mx-20 lg:mx-40'
       typeof='text'
     >
-      <h1 className='text-black  text-center py-9'>
+      <h1 className='text-indigo-800 text-center py-9'>
         Hi, Im Nicholas Quiroz, a full-stack developer with a passion for
         creating beautiful, functional, and user-friendly applications. I am
         currently looking for a full-time/intership position as a software
         engineer.
       </h1>
-      <p className='text-black text-center py-9'>
+      <p className='text-indigo-800 text-center py-9'>
         I'm self-taught, a passionate devoploper, and I have been coding for
         almost 2 years learning all I can about web development. I have
         experience with React, Next.js, Node.js, Express, MongoDB, and more. I
