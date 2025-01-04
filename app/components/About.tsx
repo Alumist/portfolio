3da2bd5d6e9969355b55c2991d6ccfbea1e63d0c
Alumist <codealumist@gmail.com>
2025-01-04 13:02:13 -0800

    error fix

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,11 +7,11 @@ function About() {
       <h1 className='text-indigo-400 text-center py-9'>
         Hi, Im Nicholas Quiroz, a full-stack developer with a passion for
         creating beautiful, functional, and user-friendly applications. I am
-        currently looking for a full-time/intership position as a software
+        currently looking for a full-time/internship position as a software
         engineer.
       </h1>
       <p className='text-indigo-400 text-center py-9'>
-        I'm self-taught, a passionate devoploper, and I have been coding for
+        I'm self-taught, a passionate developer, and I have been coding for
         almost 2 years learning all I can about web development. I have
         experience with React, Next.js, Node.js, Express, MongoDB, and more. I
         am always looking to learn more and improve my skills. I am a quick
